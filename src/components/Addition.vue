9dadb2c9ececdf4782d05a1be60a05777ca32468
Steve_Zhang <wentaozhang0305@gmail.com>
2020-05-17 20:21:08 -0700

    initial commit and set up store

---
+++ src/components/Addition.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>Counter is: {{this.$store.state.count}}</h3>
+    <button>+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Subtraction.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h3>Counter is:</h3>
+    <button>-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+</style>
